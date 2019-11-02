7f7bce0084ef97271eca9af1f3bb63ab64b228ca
Raphael Amorim <rapha850@gmail.com>
2019-11-02 17:35:09 -0300

    rewritting to rust

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+extern crate rustc_serialize;
+extern crate docopt;
+
+extern crate kyoto;
+
+use kyoto::driver::{main_loop,
+                                Tokens
+};
+
+use docopt::Docopt;
+
+const USAGE: &'static str = "
+Usage: kyoto [(-l | -p | -i)]
+Options:
+    -l  Run only lexer and show its output.
+    -p  Run only parser and show its output.
+    -i  Run only IR builder and show its output.
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    flag_l: bool,
+    flag_p: bool,
+    flag_i: bool
+}
+
+fn main() {
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
+
+    if args.flag_p || args.flag_i {
+        unimplemented!();
+    }
+    let stage = Tokens;
+
+    main_loop(stage);
+}
