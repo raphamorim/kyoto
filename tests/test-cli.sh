6aa637e1f42d460777d31fc3fffc5cba73c988cd
Raphael Amorim <rapha850@gmail.com>
2022-05-10 00:18:14 +0200

    update test

--- tests/test-cli.sh
+++ tests/test-cli.sh
@@ -1,3 +1,18 @@
+#!/bin/sh
+
+echo $SHELL
+
+contains() {
+    string="$1"
+    substring="$2"
+    if test "${string#*$substring}" != "$string"
+    then
+        return 0    # $substring is in $string
+    else
+        return 1    # $substring is not in $string
+    fi
+}
+
 throw_error() {
 	echo "\n(!) Kyoto CLI test error: \n • expected $1"
 	exit 1;	
@@ -5,6 +20,6 @@ throw_error() {
 
 CLI=$(./target/release/kyoto ./examples/sum.kto)
 
-[[ $CLI == *"Operator(\".\")"* ]] && continue || throw_error "Operator(\".\")"
-[[ $CLI == *"Ident(\"sum\")"* ]] && continue || throw_error "Ident(\"sum\")"
-[[ $CLI == *"Ident(\"log\")"* ]] && continue || throw_error "Ident(\"log\")"
+contains $CLI "Operator(\".\")" || throw_error "Operator(\".\")"
+contains $CLI "Ident(\"saum\")" || throw_error "Ident(\"sum\")"
+contains $CLI "Ident(\"log\")" || throw_error "Ident(\"log\")"
